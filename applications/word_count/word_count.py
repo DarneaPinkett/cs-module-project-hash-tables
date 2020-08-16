8084a8947cbafa08a6dea9f190a5a32ce26ac7f1
Darnea A. Pinkett <msdarneapinkett@gmail.com>
2020-08-16 12:21:36 -0400

    word ct

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,12 @@
 def word_count(s):
-    # Your code here
+    # Your code here,
+    name = s.replace('"', '').replace(':', '').replace(';','').replace(',','').replace('.','').replace('-','').replace('+','').replace('=','').replace('/','').replace('|','').replace('[','').replace(']', '').replace('{', '').replace('}', '').replace('(','').replace(')','').replace('*','').replace('^','').replace('&','').replace('\\', '')
+    if name == '':
+        return {}
+    split = name.lower().split()
+    words = {i:split.count(i) for i in split}
+    return words
+
 
 
 
